743010bd88bc29e6d78a1957c96077a8acca6ddd
mokobin <rudyrusmanto@gmail.com>
2026-05-17 21:04:49 +0700

    fix duplicate app export

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { supabase } from './lib/supabase'
 import './style.css'
 
-export default App
+
 function formatDate(dateString) {
   const date = new Date(dateString)
   const today = new Date()
